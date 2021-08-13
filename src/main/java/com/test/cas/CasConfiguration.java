7156a01c42ca26d4d52b54488e1f4dab9cee63f2
Hal Deadman <hal.deadman@gmail.com>
2021-08-12 22:36:25 -0400

    add CAS bom and exclude logback, remove thyme fixes no longer needed

--- src/main/java/com/test/cas/CasConfiguration.java
+++ src/main/java/com/test/cas/CasConfiguration.java
@@ -1,13 +1,8 @@
 package com.test.cas;
 
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CasConfiguration {
-    @Bean
-    ThymeleafProperties thymeleafProperties() {
-        return new ThymeleafProperties();
-    }
+
 }
